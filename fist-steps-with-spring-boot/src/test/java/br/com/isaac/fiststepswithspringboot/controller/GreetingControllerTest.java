bc598cf2cb909c8d13d5fbb50c62c30b36dfeccd
Isaac Batista <isaacbmaciel@gmail.com>
2023-11-23 13:03:20 -0300

    Unit Test Done and code refectoring

---
+++ fist-steps-with-spring-boot/src/test/java/br/com/isaac/fiststepswithspringboot/controller/GreetingControllerTest.java
@@ -0,0 +1,2 @@
+package br.com.isaac.fiststepswithspringboot.controller;public class GreetingControllerTest {
+}
---
+++ fist-steps-with-spring-boot/src/test/java/br/com/isaac/fiststepswithspringboot/model/GreetingModelTest.java
@@ -0,0 +1,2 @@
+package br.com.isaac.fiststepswithspringboot.model;public class GreetingModelTest {
+}
