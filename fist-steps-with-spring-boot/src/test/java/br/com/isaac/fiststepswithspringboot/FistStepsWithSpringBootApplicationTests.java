47e7c98e67e1326a985e17befa27180503251c9d
Isaac Batista <isaacbmaciel@gmail.com>
2023-11-23 02:24:49 -0300

    Class session 5 config project with springBoot

---
+++ fist-steps-with-spring-boot/src/test/java/br/com/isaac/fiststepswithspringboot/FistStepsWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.isaac.fiststepswithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FistStepsWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
