db12317b646543978288724376c4d8bf4d2b6d0f
Isaac Batista <isaacbmaciel@gmail.com>
2023-11-24 15:03:38 -0300

    Implementing math operations

---
+++ fist-steps-with-spring-boot/src/main/java/br/com/isaac/fiststepswithspringboot/controller/MathController.java
@@ -0,0 +1,48 @@
+﻿package br.com.isaac.fiststepswithspringboot.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.isaac.fiststepswithspringboot.service.MathServiceImpl;
+
+@RestController
+public class MathController implements IMathController {
+
+  private final MathServiceImpl service;
+
+  public MathController(MathServiceImpl service) {
+    this.service = service;
+  }
+
+  @Override
+  public ResponseEntity<Object> sum(@PathVariable String value1, @PathVariable String value2) {
+    return ResponseEntity.ok(service.sum(value1, value2));
+  }
+
+  @Override
+  public ResponseEntity<Object> subtraction(@PathVariable String value1, @PathVariable String value2) {
+    return ResponseEntity.ok(service.subtraction(value1, value2));
+  }
+
+  @Override
+  public ResponseEntity<Object> division(@PathVariable String value1, @PathVariable String value2) {
+    return ResponseEntity.ok(service.division(value1, value2));
+  }
+
+  @Override
+  public ResponseEntity<Object> average(@PathVariable String value1, @PathVariable String value2,
+      @PathVariable String value3, @PathVariable String value4) {
+    return ResponseEntity.ok(service.average(value1, value2, value3, value4));
+  }
+
+  @Override
+  public ResponseEntity<Object> sqrt(@PathVariable String value1) {
+    return ResponseEntity.ok(service.sqrt(value1));
+  }
+
+  @Override
+  public ResponseEntity<Object> multiplication(@PathVariable String value1, @PathVariable String value2) {
+    return ResponseEntity.ok(service.multiplication(value1, value2));
+  }
+}
---
+++ fist-steps-with-spring-boot/src/main/java/br/com/isaac/fiststepswithspringboot/service/MathService.java
@@ -0,0 +1,65 @@
+﻿package br.com.isaac.fiststepswithspringboot.service;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import br.com.isaac.fiststepswithspringboot.exceptions.UnsuportedMathOperationException;
+import br.com.isaac.fiststepswithspringboot.utils.EnumMessagesType;
+import br.com.isaac.fiststepswithspringboot.utils.Utils;
+
+@Service
+public class MathService implements MathServiceImpl {
+
+  @Override
+  public ResponseEntity<Object> sum(String value1, String value2) {
+    return performOperation(value1, value2, Utils::sum);
+  }
+
+  @Override
+  public ResponseEntity<Object> subtraction(String value1, String value2) {
+    return performOperation(value1, value2, Utils::subtraction);
+  }
+
+  @Override
+  public ResponseEntity<Object> division(String value1, String value2) {
+    return performOperation(value1, value2, Utils::division);
+  }
+
+  @Override
+  public ResponseEntity<Object> multiplication(String value1, String value2) {
+    return performOperation(value1, value2, Utils::multiplication);
+  }
+
+  @Override
+  public ResponseEntity<Object> average(String value1, String value2, String value3, String value4) {
+    try {
+      Double result = Utils.average(value1, value2, value3, value4);
+      return ResponseEntity.ok(result);
+    } catch (NumberFormatException e) {
+      throw new UnsuportedMathOperationException(EnumMessagesType.VALUE_SHOULD_BE_NUMBER.getMessage());
+    }
+  }
+
+  @Override
+  public ResponseEntity<Object> sqrt(String value1) {
+    try {
+      Double result = Utils.sqrt(value1);
+      return ResponseEntity.ok(result);
+    } catch (NumberFormatException e) {
+      throw new UnsuportedMathOperationException(EnumMessagesType.VALUE_SHOULD_BE_NUMBER.getMessage());
+    }
+  }
+
+  private interface MathOperation {
+    Double operate(String value1, String value2);
+  }
+
+  private ResponseEntity<Object> performOperation(String value1, String value2, MathOperation operation) {
+    try {
+      Double result = operation.operate(value1, value2);
+      return ResponseEntity.ok(result);
+    } catch (NumberFormatException e) {
+      throw new UnsuportedMathOperationException(EnumMessagesType.VALUE_SHOULD_BE_NUMBER.getMessage());
+    }
+  }
+}
