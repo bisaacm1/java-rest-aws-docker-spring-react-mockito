f925e108ee37f30e06e8285d0914fa23efe16572
Isaac Batista <isaacbmaciel@gmail.com>
2024-01-10 00:37:08 -0300

    criando model Person e ajustando settings para identar e organizar imports quando salvar

---
+++ fist-steps-with-spring-boot/src/main/java/br/com/isaac/fiststepswithspringboot/model/Person.java
@@ -0,0 +1,23 @@
+﻿package br.com.isaac.fiststepswithspringboot.model;
+
+import java.io.Serializable;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Person implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  private Long id;
+  private String firstName;
+  private String lastName;
+  private String adress;
+  private String gender;
+
+}
