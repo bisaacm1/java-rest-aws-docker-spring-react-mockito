850aa32444c12563e518eddb9c7560f02ce51103
Isaac Batista <isaacbmaciel@gmail.com>
2024-06-24 23:49:37 -0300

    96 Adding support migrations with flayway

---
+++ java-rest-aws-docker-spring-react-mockito/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+﻿INSERT INTO `person` (id, address, first_name, gender, last_name) VALUES
+  (4, 'Osasco - SP', 'Jair', 'Male', 'Manto'),
+  (5, 'Carapicuiba - SP', 'Ester', 'Female', 'matos'),
+  (6, 'Carapicuiba - SP', 'Ester', 'Female', 'matos'),
+  (7, 'Salvador - Bahia', 'Isaias', 'Male', 'Silva'),
+  (8, 'Carapicuiba - SP', 'Ester2', 'Female', 'matos');
